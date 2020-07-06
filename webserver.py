2b1111c8f77a98d11ec59c317fe82059cc55a295
Arkar Aung <arica.aung@gmail.com>
2020-07-07 00:22:49 +0630

    Add webserver file for canvas app

---
+++ webserver.py
@@ -0,0 +1,53 @@
+import base64
+import os
+import re
+
+from fastai.vision import *
+from flask import Flask, render_template, request
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)  # Start ngrok when app is run
+UPLOAD_DIR = 'uploads'
+OUPUT_FILE = 'output.png'
+
+
+def create_folders():
+    if not os.path.isdir(UPLOAD_DIR):
+        os.makedirs(UPLOAD_DIR)
+    if not os.path.isdir(RESULT_DIR):
+        os.makedirs(RESULT_DIR)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    parseImage(request.get_data())
+
+    # read parsed image back in 8-bit, black and white mode (L)
+    im = open_image('uploads/output.png')
+    preds_class, preds_idx, preds_output = learn.predict(im)
+    class_idx = preds_class.data.item()
+    return MAPPING[class_idx]
+
+
+def parseImage(imgData):
+    # parse canvas bytes and save as output.png
+    imgstr = re.search(b'base64,(.*)', imgData).group(1)
+    output_file = '{}/{}'.format(UPLOAD_DIR, OUPUT_FILE)
+    with open(output_file, 'wb') as output:
+        output.write(base64.decodebytes(imgstr))
+
+
+if __name__ == "__main__":
+    create_folders()
+
+    learn = load_learner('train', 'export.pkl')
+    MAPPING = {v: k for k, v in learn.data.c2i.items()}
+    app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
+    app.secret_key = 'supersecret'
+    app.run()
